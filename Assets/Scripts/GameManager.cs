6cd1cb0084fd9090d476483a8836f6c39de14895
Rayden33 <rayden33@mail.ru>
2020-12-15 20:11:15 +0500

    InitProject

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject playArea;
+    [SerializeField] private GameObject fishkaMainPrb;
+    [SerializeField] private Sprite[] fishkaFigureSpts;
+    [SerializeField] private Color[] fishkaColors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
